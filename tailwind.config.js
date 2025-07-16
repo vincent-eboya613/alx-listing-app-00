4dc0560a83745f17ad49217fe06caf2931951ad1
vincent-eboya613 <vincentkidavi@gmail.com>
2025-07-16 10:25:32 +0300

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,10 @@
-/** @type {import('postcss-load-config').Config} */
-const config = {
-  plugins: {
-    tailwindcss: {},
-    autoprefixer: {},
+module.exports = {
+  content: [
+    './pages/**/*.{js,ts,jsx,tsx}',
+    './components/**/*.{js,ts,jsx,tsx}',
+  ],
+  theme: {
+    extend: {},
   },
+  plugins: [],
 };
-
-export default config;
